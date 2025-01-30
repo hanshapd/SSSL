74d015611e47bfbf00ff2bf954d880f125852b97
hanshapd <125668433+hanshapd@users.noreply.github.com>
2025-01-30 10:58:22 +0700

    Update navmenu.js

--- navmenu.js
+++ navmenu.js
@@ -24,38 +24,3 @@ cancelBtn.addEventListener("click", () => {
     nav.classList.remove("move-up");
     desktopNav.classList.remove("move-down");
 })
-
-
-
-
-
-// Swiper Hero Section
-var listArray = ["slide1","slide2","slide3"];
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-        loop: true,
-        autoplayDisableOnInteraction: false,
-        slidesPerView: 1,        
-        autoHeight: true,
-        autoplay: {
-            delay: 3000,//animation과 시간 맞춰줘야함
-        }, 
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: 'true',
-            type: 'bullets',
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + '<em>'+ listArray[index]+'</em>' + '<i></i>' + '<b></b>'  + '</span>';
-              },
-        
-        },
-   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  
-})
